c3835c8cca515ad655a03e94d62490a17f043cb5
Tanvi <tanvi@oloid.ai>
2020-05-29 20:46:40 +0530

    Initial Commit

---
+++ OloidAdmin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OloidAdmin
+//
+//  Created by Tanvi Mittal on 29/05/20.
+//  Copyright © 2020 Tanvi Mittal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
